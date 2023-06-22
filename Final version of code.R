7322c7f65464995e5ec5ed82312bf89c60c902d5
TormodRy <137283277+TormodRy@users.noreply.github.com>
2023-06-22 12:46:59 +0200

    Add files via upload

--- Final version of code.R
+++ Final version of code.R
@@ -1,20 +1,36 @@
-##### FERDIG VERSJON. LIMER KUN INN DET SOM ER FERDIG I DENNE 
+##### Converting R to Python 
+
+# Python has a lot of packages meant to be used for data manipulation. Its main use case is still for 
+# making model. R is designed for statistics and for this reason has a really good visuals. These
+# differences result in that the capabilities of R and python are quit different. The visuals of graphs, the
+# look of the data frame are strengths of R. While these are better in python than in many other
+# languages, R has a significant advantage in these areas. This results in that trying to convert R code to
+# an equivalent in python is quit challenging, and is difficult to produce the same level of quality that R
+# is able to produce. 
+
+# To make python work as well as possible using jupyter notebook might be the best idea. The visuals in 
+# jupyter are quit good. Running python code in Rstudio can result in a bad experience as the graphs 
+# look bad. To see the data frame from the python code, Rstudios work pretty well.
+
+# For this reason i was not able to make the second half of the task work as intended. I still have
+#  provided some of the code that I have used to try to make it work. Some of the lines work, but not
+#  the entirety of the code.
 
 # things to do to make it work
 # 1) find the bf2017.dta file in compute by changing line 60(or around that)
 # 2) Recommend using Jupyter notebook to read run the code.
 #   (remember to keep the terminal running) 
-
-#First part is to install packages. If you run jupyter, do not comment them out. 
-# In python the most important packages would be pandas, numpy and matplot.
-!pip install pandas
-!pip install matplotlib
-!pip install statsmodels
-!pip install linearmodels
-!pip install pyreadstat
-!pip install datetime
-!pip install scikit-learn
-# Here I use the normal keywords such as pd, np and plt.
+# 
+# #First part is to install packages. If you run jupyter, do not comment them out if in jupyter
+# # In python the most important packages would be pandas, numpy and matplot.
+# !pip install pandas
+# !pip install matplotlib
+# !pip install statsmodels
+# !pip install linearmodels
+# !pip install pyreadstat
+# !pip install datetime
+# !pip install scikit-learn
+# # Here I use the normal keywords such as pd, np and plt.
 # SHould never be commented out
 from linearmodels.panel import PanelOLS, RandomEffects
 import matplotlib.pyplot as plt
@@ -294,59 +310,46 @@ test2(bf2017)
 # estimation with distributed-lags in levels
 # ------------------------------------------------------------------------------
 
-# Replace missing values with 0
-bf2017['D_PBD_decr'].fillna(0, inplace=True)
+def test3(bf2017):
+  
+    subset_bf2017 = bf2017.loc[bf2017['year'] >= 2012, ['log_GJSI', 'F2_D_PBD_decr', 'F1_D_PBD_decr', 'D_PBD_decr', 'L1_D_PBD_decr', 'L2_D_PBD_decr', 'L3_D_PBD_decr', 'L4_D_PBD_decr', 'frac_total_ui']]
+    x = subset_bf2017.drop(['log_GJSI'], axis=1)
+    y = subset_bf2017['log_GJSI']
+    model = sm.OLS(y, x)
+    result = model.fit()
 
-# Calculate cumulative sum by state
-bf2017['PBD_decr'] = bf2017.groupby('state')['D_PBD_decr'].cumsum()
+    summary = result.summary()
 
-# Summary statistics of PBD_decr
-summary_PBD_decr = bf2017['PBD_decr'].describe()
-print(summary_PBD_decr)
+    beta_coeffs = result.params
+    beta_coeffs = beta_coeffs[:-1]  # dropping the frac_total_ui from the list
+    std_errors = result.bse
+    std_errors = std_errors[:-1]  # dropping the frac_total_ui from the list
+    
+    x = range(-2, 5)  # Months relative to reform
+    
+    plt.figure(figsize=(8, 4))
+    
+    plt.plot(x, beta_coeffs, color='darkblue', marker='o', linestyle='-')
+    plt.errorbar(x, beta_coeffs, yerr=1.96*std_errors, color='darkblue', fmt='none')
+    plt.axhline(0, color='black')  # Horizontal line at y=0
+    plt.axvline(-0.5, color='black', linestyle='dashed')  # Vertical line at x=-0.5
+    
+    plt.xlabel("Months relative to reform")
+    plt.ylabel("Effect on log search intensity")
+    plt.title("Beta Coefficients")
+    
+    plt.xlim(-2.5, 4.5)
+    plt.ylim(-0.4, 1)
+    plt.xticks(range(-3, 5))
+    plt.yticks([-0.2, 0, 0.2, 0.4, 0.6, 0.8])
+    
+    plt.grid(True)
+    
+    plt.show()
 
-# Estimate distributed-lag model
-exog_vars = ['lead(PBD_decr, 2)', 'lead(PBD_decr, 1)', 'PBD_decr', 
-             'lag(PBD_decr, 1)', 'lag(PBD_decr, 2)', 'lag(PBD_decr, 3)', 'lag(PBD_decr, 4)',
-             'frac_total_ui', 'yearmonth']
-model_formula = 'np.log(GJSI) ~ ' + ' + '.join(exog_vars)
-model_data = bf2017[bf2017['year'] >= 2012]
-estim_decr_dl_fe = PanelOLS.from_formula(model_formula, data=model_data, effect='individual', 
-                                         entity_effects=True)
-summary_estim_decr_dl_fe = estim_decr_dl_fe.fit()
-print(summary_estim_decr_dl_fe)
+    return result
 
-# Gamma coefficients
-gamma_decr_dl_fe = summary_estim_decr_dl_fe.params[:7]
-
-# Store variance-covariance matrix
-vcov_decr_dl_fe = summary_estim_decr_dl_fe.cov[:7,:7]
-
-# Beta coefficients and standard errors
-beta_decr_dl_fe = pd.DataFrame({
-  'month_to_reform': [-3, -2, -1, 0, 1, 2, 3, 4],
-  'coef': [-np.cumsum(gamma_decr_dl_fe[:2])[::-1], 0, np.cumsum(gamma_decr_dl_fe[2:])],
-  'se': [-np.sqrt(np.cumsum(np.diag(vcov_decr_dl_fe)[:2]))[::-1], 0, np.sqrt(np.cumsum(np.diag(vcov_decr_dl_fe)[2:]))]
-})
-
-# Plot beta coefficients
-plt.figure()
-plt.errorbar(x=beta_decr_dl_fe['month_to_reform'], y=beta_decr_dl_fe['coef'], 
-             yerr=1.96*beta_decr_dl_fe['se'], color='darkblue', fmt='o')
-plt.axhline(y=0, color='black')
-plt.axvline(x=-0.5, linestyle='dashed')
-plt.xticks(np.arange(-3, 5))
-plt.xlabel("Months relative to reform \n Observations: {}, states: {}, periods: {} ({} - {})."
-           .format(estim_decr_dl_fe.nobs,
-                   estim_decr_dl_fe.model.dataframe['state'].nunique(),
-                   estim_decr_dl_fe.model.dataframe['yearmonth'].nunique(),
-                   estim_decr_dl_fe.model.dataframe['yearmonth'].min(),
-                   estim_decr_dl_fe.model.dataframe['yearmonth'].max()))
-plt.ylabel("Effect on log search intensity")
-plt.ylim(-0.045, 0.04)
-plt.yticks(np.arange(-0.04, 0.041, 0.02))
-plt.grid(True)
-plt.show()
-
+test3(bf2017)
 
 # ------------------------------------------------------------------------------
 # Figure B.1, Panel B, left 
@@ -355,71 +358,88 @@ plt.show()
 
 # overwrite treatment adoption indicators from above because of assumed zeros for NAs
 
-# Create treatment adoption indicator
-bf2017['D_PBD_incr'] = np.where(np.diff(bf2017['PBD'], 1) >= 13, 1, 0)
-
-# Create 3 leads
-bf2017['F3_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(-3)
-bf2017['F2_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(-2)
-bf2017['F1_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(-1)
-
-# Create 4 lags
-bf2017['L1_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(1)
-bf2017['L2_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(2)
-bf2017['L3_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(3)
-bf2017['L4_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(4)
-
-# Set arbitrary value for the 4th lag of the treatment adoption indicator for 2006-05
-bf2017.loc[bf2017['yearmonth'] == '2006-05', 'L4_D_PBD_incr'] = 0
-
-# Define crisis sample: 2006-05 to 2011-12
-bf2017['crisis'] = (pd.to_datetime(bf2017['yearmonth']).dt.to_period('M') >= pd.Period('2006-05')) & \
-(pd.to_datetime(bf2017['yearmonth']).dt.to_period('M') <= pd.Period('2011-12'))
-
-# Generate binned endpoints according to eq. (5)
-bf2017['L4bin_D_PBD_incr'] = bf2017.groupby(['state', 'crisis'])['L4_D_PBD_incr'].cumsum()
-
-# Generate binned endpoint at lead 3 according to eq. (5)
-bf2017['F3bin_D_PBD_incr'] = bf2017.groupby(['state', 'crisis'])['F3_D_PBD_incr'].cumsum()[::-1]
-
-# Estimate event-study model in levels with fixed effects
-exog_vars = ['F3bin_D_PBD_incr', 'F2_D_PBD_incr', 'D_PBD_incr', 'L1_D_PBD_incr', 
-             'L2_D_PBD_incr', 'L3_D_PBD_incr', 'L4bin_D_PBD_incr', 'frac_total_ui', 'yearmonth']
-model_formula = 'np.log(GJSI) ~ ' + ' + '.join(exog_vars)
-model_data = bf2017[bf2017['year'] <= 2011]
-estim_incr_es_fe = PanelOLS.from_formula(model_formula, data=model_data, effect='individual', 
-                                         entity_effects=True)
-summary_estim_incr_es_fe = estim_incr_es_fe.fit()
-print(summary_estim_incr_es_fe)
-
-# Beta coefficients and standard errors
-results_incr_es_fe = summary_estim_incr_es_fe.coefs
-beta_incr_es_fe = pd.DataFrame({
-  'month_to_reform': [-3, -2, -1, 0, 1, 2, 3, 4],
-  'coef': [results_incr_es_fe[1:3]['coefficient'].values, 0, results_incr_es_fe[3:]['coefficient'].values],
-  'se': [results_incr_es_fe[1:3]['std err'].values, 0, results_incr_es_fe[3:]['std err'].values]
-})
-
-# Plot beta coefficients
-plt.figure()
-plt.errorbar(x=beta_incr_es_fe['month_to_reform'], y=beta_incr_es_fe['coef'], 
-             yerr=1.96*beta_incr_es_fe['se'], color='darkblue', fmt='o')
-plt.axhline(y=0, color='black')
-plt.axvline(x=-0.5, linestyle='dashed')
-plt.xticks(np.arange(-3, 5))
-plt.xlabel("Months relative to reform \n Observations: {}, states: {}, periods: {} ({} - {})."
-           .format(estim_incr_es_fe.nobs,
-                   estim_incr_es_fe.model.dataframe['state'].nunique(),
-                   estim_incr_es_fe.model.dataframe['yearmonth'].nunique(),
-                   estim_incr_es_fe.model.dataframe['yearmonth'].min(),
-                   estim_incr_es_fe.model.dataframe['yearmonth'].max()))
-plt.ylabel("Effect on log search intensity")
-plt.ylim(-0.06, 0.025)
-plt.yticks(np.arange(-0.06, 0.021, 0.02))
-plt.grid(True)
-plt.show()
+# # Create treatment adoption indic ator
+# bf2017['D_PBD_incr'] = np.where(np.diff(bf2017['PBD'], 1) >= 13, 1, 0)
+# 
+# # Create 3 leads
+# bf2017['F3_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(-3)
+# bf2017['F2_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(-2)
+# bf2017['F1_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(-1)
+# 
+# # Create 4 lags
+# bf2017['L1_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(1)
+# bf2017['L2_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(2)
+# bf2017['L3_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(3)
+# bf2017['L4_D_PBD_incr'] = bf2017['D_PBD_incr'].shift(4)
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from linearmodels import PanelOLS
+import matplotlib.pyplot as plt
 
 
+def test4(bf2017):
+    bf2017['yearmonth'] = pd.to_datetime(bf2017['yearmonth'])
+    # print(bf2017.isnull().sum())
+    # Set arbitrary value for the 4th lag of the treatment adoption indicator for 2006-05
+    # subset_bf2017[GJSI] = bf2017[GJSI]
+    # test4 was not possible to get working. I dont know what to do
+    bf2017.loc[bf2017['yearmonth'] == '2006-05', 'L4_D_PBD_incr'] = 0
+    print(bf2017.columns)
+    # Define crisis sample: 2006-05 to 2011-12
+    bf2017['crisis'] = (pd.to_datetime(bf2017['yearmonth']).dt.to_period('M') >= pd.Period('2006-05')) & \
+    (pd.to_datetime(bf2017['yearmonth']).dt.to_period('M') <= pd.Period('2011-12'))
+
+    # Generate binned endpoints according to eq. (5)
+    bf2017['L4bin_D_PBD_incr'] = bf2017.groupby(['state', 'crisis'])['L4_D_PBD_incr'].cumsum()
+
+    # Generate binned endpoint at lead 3 according to eq. (5)
+    bf2017['F3bin_D_PBD_incr'] = bf2017.groupby(['state', 'crisis'])['F3_D_PBD_incr'].cumsum()[::-1]
+
+    # Estimate event-study model in levels with fixed effects
+    exog_vars = ['F3bin_D_PBD_incr', 'F2_D_PBD_incr', 'D_PBD_incr', 'L1_D_PBD_incr',
+                 'L2_D_PBD_incr', 'L3_D_PBD_incr', 'L4bin_D_PBD_incr', 'frac_total_ui', 'yearmonth']
+    y_var = bf2017['log_GJSI']
+
+    subset_bf2017 = bf2017.loc[bf2017['year'] >= 2012, ['log_GJSI', 'F2_D_PBD_decr', 'F1_D_PBD_decr', 'D_PBD_decr', 'L1_D_PBD_decr', 'L2_D_PBD_decr', 'L3_D_PBD_decr', 'L4_D_PBD_decr', 'frac_total_ui']]
+    x = subset_bf2017.drop(['log_GJSI'], axis=1)
+    y = subset_bf2017['log_GJSI']
+    model = sm.OLS(y_var, exog_vars)
+    result = model.fit()
+
+    # model_data = bf2017[bf2017['year'] <= 2011]
+    estim_incr_es_fe = PanelOLS.from_formula(model, data=subset_bf2017)
+    summary_estim_incr_es_fe = estim_incr_es_fe.fit()
+    print(summary_estim_incr_es_fe)
+
+    # Beta coefficients and standard errors
+    results_incr_es_fe = summary_estim_incr_es_fe.coefs
+    beta_incr_es_fe = pd.DataFrame({
+      'month_to_reform': [-3, -2, -1, 0, 1, 2, 3, 4],
+      'coef': [results_incr_es_fe[1:3]['coefficient'].values, 0, results_incr_es_fe[3:]['coefficient'].values],
+      'se': [results_incr_es_fe[1:3]['std err'].values, 0, results_incr_es_fe[3:]['std err'].values]
+    })
+
+    # Plot beta coefficients
+    plt.figure()
+    plt.errorbar(x=beta_incr_es_fe['month_to_reform'], y=beta_incr_es_fe['coef'],
+                 yerr=1.96*beta_incr_es_fe['se'], color='darkblue', fmt='o')
+    plt.axhline(y=0, color='black')
+    plt.axvline(x=-0.5, linestyle='dashed')
+    plt.xticks(np.arange(-3, 5))
+    plt.xlabel("Months relative to reform \n Observations: {}, states: {}, periods: {} ({} - {})."
+               .format(estim_incr_es_fe.nobs,
+                       estim_incr_es_fe.model.dataframe['state'].nunique(),
+                       estim_incr_es_fe.model.dataframe['yearmonth'].nunique(),
+                       estim_incr_es_fe.model.dataframe['yearmonth'].min(),
+                       estim_incr_es_fe.model.dataframe['yearmonth'].max()))
+    plt.ylabel("Effect on log search intensity")
+    plt.ylim(-0.06, 0.025)
+    plt.yticks(np.arange(-0.06, 0.021, 0.02))
+    plt.grid(True)
+    plt.show()
+
+test4(bf2017)
 
 # ------------------------------------------------------------------------------
 # Figure B.2, left
@@ -434,7 +454,7 @@ plt.show()
 exog_vars = ['lead(PBD, 2)', 'lead(PBD, 1)', 'PBD', 'lag(PBD, 1)', 'lag(PBD, 2)', 'lag(PBD, 3)', 'lag(PBD, 4)', 'frac_total_ui', 'yearmonth']
 model_formula = 'np.log(GJSI) ~ ' + ' + '.join(exog_vars)
 model_data = bf2017.dropna(subset=exog_vars + ['GJSI'])
-estim_dl_fe_full = PanelOLS.from_formula(model_formula, data=model_data, effect='individual', 
+estim_dl_fe_full = PanelOLS.from_formula(model_formula, data=model_data, effect='individual',
                                          entity_effects=True)
 summary_estim_dl_fe_full = estim_dl_fe_full.fit()
 print(summary_estim_dl_fe_full)
@@ -449,7 +469,7 @@ vcov_dl_fe_full = summary_estim_dl_fe_full.cov[:7, :7]
 beta_dl_fe_full = pd.DataFrame({
   'month_to_reform': list(range(-3, 5)),
   'coef': [-np.cumsum(gamma_dl_fe_full[:2])[::-1], 0, np.cumsum(gamma_dl_fe_full[2:])],
-  'se': [np.sqrt(np.cumsum(np.diag(vcov_dl_fe_full)[:2]))[::-1], 0, 
+  'se': [np.sqrt(np.cumsum(np.diag(vcov_dl_fe_full)[:2]))[::-1], 0,
          np.sqrt(np.cumsum(np.diag(vcov_dl_fe_full)[2:]))]
 })
 
@@ -457,7 +477,7 @@ beta_dl_fe_full = pd.DataFrame({
 plt.figure()
 plt.plot(beta_dl_fe_full['month_to_reform'], beta_dl_fe_full['coef'], color='darkblue')
 plt.scatter(beta_dl_fe_full['month_to_reform'], beta_dl_fe_full['coef'], color='darkblue')
-plt.errorbar(x=beta_dl_fe_full['month_to_reform'], y=beta_dl_fe_full['coef'], 
+plt.errorbar(x=beta_dl_fe_full['month_to_reform'], y=beta_dl_fe_full['coef'],
              yerr=1.96*beta_dl_fe_full['se'], color='darkblue', fmt='o')
 plt.axhline(y=0, color='black')
 plt.axvline(x=-0.5, linestyle='dashed')
@@ -478,7 +498,7 @@ plt.show()
 plt.figure(figsize=(8, 4))
 plt.plot(beta_dl_fe_full['month_to_reform'], beta_dl_fe_full['coef'], color='darkblue')
 plt.scatter(beta_dl_fe_full['month_to_reform'], beta_dl_fe_full['coef'], color='darkblue')
-plt.errorbar(x=beta_dl_fe_full['month_to_reform'], y=beta_dl_fe_full['coef'], 
+plt.errorbar(x=beta_dl_fe_full['month_to_reform'], y=beta_dl_fe_full['coef'],
              yerr=1.96*beta_dl_fe_full['se'], color='darkblue', fmt='o')
 plt.axhline(y=0, color='black')
 plt.axvline(x=-0.5, linestyle='dashed')
@@ -525,7 +545,7 @@ vcov_dl_fe_crisis = summary_estim_dl_fe_crisis.cov[:7, :7]
 beta_dl_fe_crisis = pd.DataFrame({
   'month_to_reform': list(range(-3, 5)),
   'coef': [-np.cumsum(gamma_dl_fe_crisis[:2])[::-1], 0, np.cumsum(gamma_dl_fe_crisis[2:])],
-  'se': [np.sqrt(np.cumsum(np.diag(vcov_dl_fe_crisis)[:2]))[::-1], 0, 
+  'se': [np.sqrt(np.cumsum(np.diag(vcov_dl_fe_crisis)[:2]))[::-1], 0,
          np.sqrt(np.cumsum(np.diag(vcov_dl_fe_crisis)[2:]))]
 })
 
@@ -533,7 +553,7 @@ beta_dl_fe_crisis = pd.DataFrame({
 plt.figure()
 plt.plot(beta_dl_fe_crisis['month_to_reform'], beta_dl_fe_crisis['coef'], color='darkblue')
 plt.scatter(beta_dl_fe_crisis['month_to_reform'], beta_dl_fe_crisis['coef'], color='darkblue')
-plt.errorbar(x=beta_dl_fe_crisis['month_to_reform'], y=beta_dl_fe_crisis['coef'], 
+plt.errorbar(x=beta_dl_fe_crisis['month_to_reform'], y=beta_dl_fe_crisis['coef'],
              yerr=1.96*beta_dl_fe_crisis['se'], color='darkblue', fmt='o')
 plt.axhline(y=0, color='black')
 plt.axvline(x=-0.5, linestyle='dashed')
@@ -587,7 +607,7 @@ vcov_dl_fd_crisis = summary_estim_dl_fd_crisis.cov[:8, :8]
 beta_dl_fd_crisis = pd.DataFrame({
   'month_to_reform': list(range(-3, 5)),
   'coef': [-np.cumsum(gamma_dl_fd_crisis[:2])[::-1], 0, np.cumsum(gamma_dl_fd_crisis[2:])],
-  'se': [np.sqrt(np.cumsum(np.diag(vcov_dl_fd_crisis)[:2]))[::-1], 0, 
+  'se': [np.sqrt(np.cumsum(np.diag(vcov_dl_fd_crisis)[:2]))[::-1], 0,
          np.sqrt(np.cumsum(np.diag(vcov_dl_fd_crisis)[2:]))]
 })
 
@@ -604,7 +624,7 @@ for estimator, color, shape in zip(['Fixed effects', 'First difference'], colors
   subset = beta_dl_fefd_crisis[beta_dl_fefd_crisis['estimator'] == estimator]
 plt.plot(subset['month_to_reform'], subset['coef'], color=color)
 plt.scatter(subset['month_to_reform'], subset['coef'], color=color, marker=shape)
-plt.errorbar(x=subset['month_to_reform'], y=subset['coef'], 
+plt.errorbar(x=subset['month_to_reform'], y=subset['coef'],
              yerr=1.96*subset['se'], color=color, fmt=shape)
 plt.axhline(y=0, color='black')
 plt.axvline(x=-0.5, linestyle='dashed')
@@ -629,7 +649,7 @@ for estimator, color, shape in zip(['Fixed effects', 'First difference'], colors
   subset = beta_dl_fefd_crisis[beta_dl_fefd_crisis['estimator'] == estimator]
 plt.plot(subset['month_to_reform'], subset['coef'], color=color)
 plt.scatter(subset['month_to_reform'], subset['coef'], color=color, marker=shape)
-plt.errorbar(x=subset['month_to_reform'], y=subset['coef'], 
+plt.errorbar(x=subset['month_to_reform'], y=subset['coef'],
              yerr=1.96*subset['se'], color=color, fmt=shape)
 plt.axhline(y=0, color='black')
 plt.axvline(x=-0.5, linestyle='dashed')
@@ -646,6 +666,7 @@ plt.yticks(np.arange(-0.01, 0.003, 0.002))
 plt.grid(True)
 plt.savefig("./Fig_B4_left.pdf", format='pdf')
 plt.close()
+
 
 
 
